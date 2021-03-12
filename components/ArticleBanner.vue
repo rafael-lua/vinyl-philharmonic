b7796ae3fd2e9a1322f360ad539d3c6ed5d77d61
Rafael <rafael_oliveira@gmx.com>
2021-03-12 19:13:36 -0300

    Updated article banner style and structure

--- components/ArticleBanner.vue
+++ components/ArticleBanner.vue
@@ -2,6 +2,7 @@
   
   <div class="container article-banner">
     <div class="img-side">
+      <img src="~/assets/article-1.jpg" alt="Article 1 - A violin">
       <ul class="dot-container">
         <nuxt-link to="/"><li class="dot-button"></li></nuxt-link>
         <nuxt-link to="/"><li class="dot-button"></li></nuxt-link>
@@ -10,7 +11,9 @@
       </ul>
     </div>
     <div class="text-side">
-
+      <h1>TITLE</h1>
+      <h2>Description</h2>
+      <button class="article-button"> READ MORE</button>
     </div>
   </div>
   
@@ -31,6 +34,7 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
+  overflow-y: hidden;
 }
 
 .dot-container {
@@ -44,14 +48,41 @@ export default {
   margin: 0 1em;
   width: 1em;
   height: 1em;
-  border: 2px solid rgba(0, 0, 0, 0.7);
+  border: 2px solid rgba(255, 255, 255, 0.2);
   border-radius: 50%;
-  background-color: rgba(255, 255, 255, 0.7);
+  background-color: rgba(255, 255, 255, 0.2);
 }
 
 .dot-button:hover {
   border: 1px solid rgba(0, 0, 0, 0.5);
   background-color: rgba(255, 255, 255, 0.95);
+}
+
+.text-side {
+  text-align: right;
+  padding: 1em;
+}
+
+.text-side h1 {
+  font-weight: 700;
+  font-size: 3em;
+}
+
+.article-button {
+  position: absolute;
+  padding: 0.55em;
+  bottom: 1em;
+  right: 1em;
+  background-color: rgba(0, 0, 0, 0);
+  color: var(--link-color);
+  border: 1px solid var(--link-color);
+  border-radius: 5px;
+  font-weight: 700;
+}
+
+.article-button:hover {
+  background-color: var(--link-color-focused);
+  color: var(--primary-color);
 }
 
 </style>
