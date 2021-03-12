12a9ea095319cdfe9118faca80ba0ba2ca813c5b
Rafael <rafael_oliveira@gmx.com>
2021-03-12 15:09:59 -0300

    Added global css (+ minor changes)

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,65 +6,7 @@
 </template>
 
 
+
 <style>
-
-/* font-family: 'Lato', sans-serif; */
-
-* {
-  box-sizing: border-box;
-  padding: 0;
-  margin: 0;
-}
-
-body {
-  font-family: 'Lato', sans-serif;
-  color: #333;
-  line-height: 1.6;
-}
-
-ul {
-  list-style-type: none;
-}
-
-a {
-  text-decoration: none;
-  color: #333;
-}
-
-h1, h2 {
-  font-weight: 300;
-  line-height: 1.2;
-  margin: 10px 0; /* Top and bottom margin */
-}
-
-p {
-  margin: 10px 0;
-}
-
-img {
-  width: 100%;
-}
-
-code, pre {
-  background: #333;
-  color: #fff;
-  padding: 10px;
-}
-
-.container {
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 5px 5px;
-}
-
-/* Smaller devices breakpoint */
-@media only screen and (max-width: 768px) {
-
-}
-
-/* Bigger devices breakpoint */
-@media only screen and (min-width: 768px) {
-  
-}
 
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Orchestra and Music School' }
+      { hid: 'description', name: 'description', content: 'Orchestra and Music School' },
+      {  }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -22,6 +23,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    "~/assets/css/main.css"
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
