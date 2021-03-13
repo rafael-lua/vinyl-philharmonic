f11855110f328915646ed1f75de25f7cecb2d8cf
Rafael <rafael_oliveira@gmx.com>
2021-03-13 17:58:32 -0300

    updated dist

--- dist/_nuxt/static/1615668280/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fvinyl-philharmonic\u002Fdist\u002F_nuxt\u002Fstatic\u002F1615668280",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fvinyl-philharmonic\u002Fdist\u002F",assetsPath:"\u002Fvinyl-philharmonic\u002Fdist\u002F_nuxt\u002F",cdnURL:a}}}}(null));
